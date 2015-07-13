366eed54b92647dcb723c836d6c0b70a4ca36ea5
zhu.yuanzhe <zhu.yuanzhe@zhimatech.com>
2015-07-13 10:35:05 +0800

    添加文件支持

--- src/Zjango/Sendcloud/Sendcloud.php
+++ src/Zjango/Sendcloud/Sendcloud.php
@@ -24,28 +24,47 @@ class Sendcloud{
         return 0;
     }
 
-    public function doSend($to,$subject,$content)
+    public function doSend($to,$subject,$content,$files='')
     {
         $sendcloud_config=\Config::get('mail.sendcloud');
-        $url = 'http://sendcloud.sohu.com/webapi/mail.send.json';
-        $param = array(
-            'api_user' => $sendcloud_config['api_user'],
-            'api_key' => $sendcloud_config['api_key'],
-            'from' => $sendcloud_config['from_addr'],
-            'fromname' => $sendcloud_config['from_name'],
-            'to' => $to,
-            'subject' => $subject,
-            'html' => $content
-        );
-        $options = array(
-            'http' => array(
-                'method' => 'POST',
-                'header' => "Content-type: application/x-www-form-urlencoded ",
-                'content' => http_build_query($param)
-            )
-        );
-        $context = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
+        $ch = curl_init();
+        if(!empty($files))
+        {
+            if (class_exists('\CURLFile')) {
+                curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
+            } else {
+                if (defined('CURLOPT_SAFE_UPLOAD')) {
+                    curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false);
+                }
+            }
+
+            if (class_exists('\CURLFile')) {
+                $files = new \CURLFile(realpath($files));
+            } else {
+                $files = '@' . realpath($files);
+            }          
+        }
+
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_URL, 'http://sendcloud.sohu.com/webapi/mail.send.json');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+                                'api_user' => $sendcloud_config['api_user'], # 使用api_user和api_key进行验证
+                                'api_key' => $sendcloud_config['api_key'],
+                                'from' => $sendcloud_config['from_addr'], # 发信人，用正确邮件地址替代
+                                'fromname' => $sendcloud_config['from_name'],
+                                'to' => $to, # 收件人地址，用正确邮件地址替代，多个地址用';'分隔
+                                'subject' => $subject,
+                                'html' => $content,
+                                'files' => $files
+                            ));
+
+        $result = curl_exec($ch);
+        if($result === false) {
+            return json_encode(['message'=>'error','errors'=>'Request error!']);
+        }
+        curl_close($ch);
         return $result;
     }
 }
